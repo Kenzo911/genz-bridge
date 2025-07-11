fdd5f58c1718e512ffec23ca7903b9876ae8daa0
kenzonayandra <kenzonayandra@gmail.com>
2025-07-11 12:23:19 +0700

    refactor: cors fix attempt, env=prod

--- server/scripts/start_docker_container.sh
+++ server/scripts/start_docker_container.sh
@@ -5,6 +5,7 @@ echo "Starting new container..."
 OPENAI_API_KEY_SSM=$(aws ssm get-parameter --name "/genzbridge/openai_api_key" --with-decryption --query Parameter.Value --output text)
 DATABASE_URL_SSM=$(aws ssm get-parameter --name "/genzbridge/database_url" --with-decryption --query Parameter.Value --output text)
 DOCKER_USERNAME_SSM=$(aws ssm get-parameter --name "/genzbridge/docker_username" --query Parameter.Value --output text)
+NODE_ENV_PROD="production"
 
 if [ -z "$OPENAI_API_KEY_SSM" ] || [ -z "$DATABASE_URL_SSM" ] || [ -z "$DOCKER_USERNAME_SSM" ]; then
   echo "Error: Failed to retrieve one or more parameters from SSM Parameter Store. Ensure parameters exist and IAM role has permissions."
@@ -19,6 +20,7 @@ docker run -d \
   -e DATABASE_URL="${DATABASE_URL_SSM}" \
   -e OPENAI_API_KEY="${OPENAI_API_KEY_SSM}" \
   -e PORT=5001 \
+  -e NODE_ENV="${NODE_ENV_PROD}" \
   "${DOCKER_USERNAME_SSM}"/genz-bridge-backend:latest
 
 echo "New container started."
